1a383f54802a97978de85c1382b812d5d0a33c62
Ray <raytang11792@gmail.com>
2023-11-05 18:27:58 -0500

    js

---
+++ textAnimation.js
@@ -0,0 +1,26 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()[]{};':<>?/";
+let interval = null;
+
+document.querySelector(".nameheader").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText.split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * letters.length)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
